52e3b306b80af4d8a0a82aa01bdceacc2cfa9b62
milanosherzodgold <milanosherzodgold@gmail.com>
2026-03-04 19:42:40 +0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app.add_middleware(
 )
 
 # Mongo ENV
-MONGO_URL = os.getenv("MONGO_URL") or "mongodb+srv://milanosherzodgold_db_user:Sherzod7$@cluster0.ftuoneb.mongodb.net/?appName=Cluster0"
+MONGO_URL = os.getenv("MONGO_URL") or "mongodb://localhost:27017/"
 DB_NAME = os.getenv("DB_NAME", "sklad_db")
 
 client = MongoClient(MONGO_URL)
@@ -222,4 +222,5 @@ def report_monthly(year: int, month: int):
         "profit": a["profit"],
         "salesCount": a["count"]
     }
+
 
